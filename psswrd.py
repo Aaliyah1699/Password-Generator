b3f8b2e5845ce30a0484a29fe044637425ffb30a
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-04 03:46:42 +0000

    Used string mod constants

--- psswrd.py
+++ psswrd.py
@@ -1,4 +1,10 @@
 '''Password Generator using secrets and string modules to generate a strong passwords'''
-import secrets
-import string
+import secrets  # obtains all possible letters, digits, and special characters
+import string  # obtain cryptographically secure passwords
 
+#function to create password at least 10 characters long
+
+def create_pw(pw_length=10):
+    letters = string.ascii_letters #contains all letters a-z
+    nums = string.digits #for digits in the pw
+    characters = string.punctuation #special characters
