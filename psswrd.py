2f84345cc5406ef0ea77f43af46a70dfd07e0de8
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-04 03:55:16 +0000

    Finished func

--- psswrd.py
+++ psswrd.py
@@ -8,3 +8,17 @@ def create_pw(pw_length=10):
     letters = string.ascii_letters #contains all letters a-z
     nums = string.digits #for digits in the pw
     characters = string.punctuation #special characters
+
+    alphabet = letters + nums + characters #add char in pw together
+    pwd = ''
+    strong_pw = False
+    while not strong_pw:
+        pwd = ''
+        for i in range(pwd_len):
+            pwd += ''.join(secrets.choice(alphabet))
+        if (any(char in characters for char in pwd) and sum(char in nums for char in pwd)>= 2):
+            strong_pw = True
+    return pwd
+
+if __name__ == '__main__':
+    print(create_pw())
